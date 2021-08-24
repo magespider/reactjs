8ed39954616f67e2849578e8509fb5ae762da1d8
mores.com.tw <bitbucket.ms@mores.com.tw>
2021-08-24 12:28:22 +0530

    changes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -19,8 +19,8 @@ function Home() {
                 <section className="py-5 text-center container">
                     <div className="row py-lg-5">
                         <div className="col-lg-6 col-md-8 mx-auto">
-                        <h1 className="fw-light">dfgdfg</h1>
-                        <p className="lead text-muted">fghfgh</p>
+                        <h1 className="fw-light">Home Page</h1>
+                        <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
                         <p>
                             <a href="https://github.com/" className="btn btn-primary my-2">Main call to action</a>
                             <a href="https://github.com/" className="btn btn-secondary my-2">Secondary action</a>
